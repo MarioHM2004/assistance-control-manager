37528bf32146c710036de29e3435b3ee63da8c79
MarioHM2004 <mario-alessandro.herranz-machado@epitech.eu>
2024-10-10 13:36:09 +0200

    add: for `layout`: `navbar` and ``sidebar`

---
+++ app/src/components/navBar/SideBar.tsx
@@ -0,0 +1,45 @@
+import { Link } from 'react-router-dom';
+import { MdOutlineTableRows } from 'react-icons/md';
+import { MdOutlineDisabledByDefault } from 'react-icons/md';
+import { GrUserWorker } from "react-icons/gr";
+
+export const SideBar: React.FC = () => {
+  return (
+    <div className="drawer">
+      <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-side z-10">
+        <label
+          htmlFor="my-drawer"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+        <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4 flex flex-col items-center">
+          {/* Sidebar content here */}
+          <img
+            className="max-w-40 max-h-40 pb-4"
+            src="/diselLogo.png"
+            alt="logo de Disel Studio SL"
+          />
+          <p className="font-bold pb-2">ACM</p>
+          <li className="self-start w-full">
+            <div className="flex items-center space-x-2">
+              <MdOutlineDisabledByDefault stroke="0.1" size={20} />
+              <Link to="/Absence">Nueva falta</Link>
+            </div>
+            <div className="flex items-center space-x-2">
+              <MdOutlineTableRows size={20} />
+              <Link to="/dashboard">Tablero</Link>
+            </div>
+            <div className="flex items-center space-x-2">
+              <GrUserWorker size={20}/>
+              <Link to="/employees">Empleados</Link>
+            </div>
+            <div>
+              <Link to="/login">Login</Link>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
