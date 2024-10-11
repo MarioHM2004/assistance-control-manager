3594cf7329b39f3a1ce0706c3d189069545cdfb0
MarioHM2004 <mario-alessandro.herranz-machado@epitech.eu>
2024-10-11 14:04:41 +0200

    add: `contextBridge` for `ipcRenderer` methods

--- preload.js
+++ preload.js
@@ -1,6 +1,7 @@
 const { contextBridge, ipcRenderer } = require('electron');
-const path = require('path');
 
 contextBridge.exposeInMainWorld('electron', {
-
+  ipcRenderer: {
+    invoke: (channel, ...args) => ipcRenderer.invoke(channel, ...args),
+  },
 });
