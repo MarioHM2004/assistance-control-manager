0847a0f5520262c3b34415337461b721de25020f
MarioHM2004 <mario-alessandro.herranz-machado@epitech.eu>
2024-11-14 11:58:35 +0100

    add: `edit` and `delete` absence

--- app/src/components/tables/AbsenceTable.tsx
+++ app/src/components/tables/AbsenceTable.tsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { Modal } from 'react-daisyui';
 
 interface Absence {
+  absenceId: number;
   name: string;
   absenceType: string;
   description: string;
@@ -9,8 +11,14 @@ interface Absence {
   date: string;
 }
 
+interface AbsenceType {
+  ABSENCE_TYPE_ID: number;
+  TYPE: string;
+}
+
 const AssistTable: React.FC = () => {
   const [data, setData] = useState<Absence[]>([]);
+  const [absenceTypes, setAbsenceTypes] = useState<AbsenceType[]>([]);
   const [filters, setFilters] = useState({
     name: '',
     absenceType: '',
@@ -18,6 +26,8 @@ const AssistTable: React.FC = () => {
     hoursAbsent: '',
     date: '',
   });
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [editingAbsence, setEditingAbsence] = useState<Absence | null>(null);
 
   useEffect(() => {
     const fetchAbsences = async () => {
@@ -25,7 +35,11 @@ const AssistTable: React.FC = () => {
         const result: Absence[] = await window.electron.ipcRenderer.invoke(
           'get-absences'
         );
+        const typesResponse = await window.electron.ipcRenderer.invoke(
+          'get-absence-types'
+        );
         setData(result);
+        setAbsenceTypes(typesResponse);
       } catch (error) {
         console.error('Error fetching absences:', error);
       }
@@ -55,10 +69,68 @@ const AssistTable: React.FC = () => {
     });
 
     const matchesHoursAbsent =
-      !filters.hoursAbsent || item.hoursAbsent === parseFloat(filters.hoursAbsent);
+      !filters.hoursAbsent ||
+      item.hoursAbsent === parseFloat(filters.hoursAbsent);
 
     return matchesFilters && matchesHoursAbsent;
   });
+
+  const handleEditAbsence = (absence: Absence) => {
+    setEditingAbsence(absence);
+    setIsModalOpen(true);
+  };
+
+  const handleDeleteAbsence = async (absenceId: number) => {
+    try {
+      const result = await window.electron.ipcRenderer.invoke(
+        'delete-absence',
+        absenceId
+      );
+      if (result > 0) {
+        setData(data.filter((absence) => absence.absenceId !== absenceId));
+      }
+    } catch (error) {
+      console.error('Error deleting absence:', error);
+    }
+  };
+
+  const handleUpdateAbsence = async () => {
+    if (editingAbsence) {
+      const selectedAbsenceType = absenceTypes.find(
+        (type) => type.TYPE === editingAbsence.absenceType
+      );
+
+      if (!selectedAbsenceType) {
+        console.error('Tipo de falta no encontrado para actualización');
+        return;
+      }
+
+      const absenceData = {
+        ...editingAbsence,
+        absenceTypeId: selectedAbsenceType.ABSENCE_TYPE_ID,
+      };
+
+      try {
+        const result = await window.electron.ipcRenderer.invoke(
+          'edit-absence',
+          absenceData
+        );
+        if (result > 0) {
+          setData(
+            data.map((absence) =>
+              absence.absenceId === editingAbsence.absenceId
+                ? editingAbsence
+                : absence
+            )
+          );
+          setIsModalOpen(false);
+          setEditingAbsence(null);
+        }
+      } catch (error) {
+        console.error('Error updating absence:', error);
+      }
+    }
+  };
 
   return (
     <div className="container mx-auto pt-8 xl:pr-16 xl:pl-16 sm:pl-2 pb-8">
@@ -77,12 +149,6 @@ const AssistTable: React.FC = () => {
           </Link>
           <Link
             to="/Absence"
-            className="btn btn-xs sm:btn-sm md:btn-md lg:btn-md btn-secondary mr-2"
-          >
-            Importar
-          </Link>
-          <Link
-            to="/Absence"
             className="btn btn-xs sm:btn-sm md:btn-md lg:btn-md btn-secondary"
           >
             Exportar
@@ -94,102 +160,106 @@ const AssistTable: React.FC = () => {
           <thead className="sticky top-0 bg-base-300">
             <tr>
               <th className="w-1/5">
-                <div className="flex items-center justify-between">
-                  <span className="font-extrabold text-base">Nombre</span>
+                <div>
+                  <p className="pb-2 font-extrabold text-base">Nombre</p>
                   <input
                     type="text"
-                    placeholder="Filtro"
-                    className="input input-bordered input-sm w-36 ml-2"
                     name="name"
+                    placeholder="Filtrar por nombre"
                     value={filters.name}
                     onChange={handleFilterChange}
+                    className="input input-bordered w-full h-8"
                   />
                 </div>
               </th>
               <th className="w-1/5">
-                <div className="flex items-center justify-between">
-                  <span className="font-extrabold text-base">
-                    Tipo de Falta
-                  </span>
+                <div>
+                  <p className="pb-2 font-extrabold text-base">Tipo de Falta</p>
                   <input
                     type="text"
-                    placeholder="Filtro"
-                    className="input input-bordered input-sm w-36 ml-2"
                     name="absenceType"
+                    placeholder="Filtrar por tipo de falta"
                     value={filters.absenceType}
                     onChange={handleFilterChange}
+                    className="input input-bordered w-full h-8"
                   />
                 </div>
               </th>
               <th className="w-1/5">
-                <div className="flex items-center justify-between">
-                  <span className="font-extrabold text-base">Descripción</span>
+                <div>
+                  <p className="pb-2 font-extrabold text-base">Descripción</p>
                   <input
                     type="text"
-                    placeholder="Filtro"
-                    className="input input-bordered input-sm w-36 ml-2"
                     name="description"
+                    placeholder="Filtrar por descripción"
                     value={filters.description}
                     onChange={handleFilterChange}
+                    className="input input-bordered w-full h-8"
                   />
                 </div>
               </th>
               <th className="w-1/5">
-                <div className="flex items-center justify-between">
-                  <span className="font-extrabold text-base">
+                <div>
+                  <p className="pb-2 font-extrabold text-base">
                     Horas Faltadas
-                  </span>
-                  <input
-                    type="number"
-                    placeholder="Filtro"
-                    className="input input-bordered input-sm w-32"
-                    name="hoursAbsent"
-                    value={filters.hoursAbsent}
-                    onChange={handleFilterChange}
-                  />
-                </div>
-              </th>
-              <th className="w-1/5">
-                <div className="flex items-center justify-between">
-                  <span className="font-extrabold text-base">Fecha</span>
+                  </p>
                   <input
                     type="text"
-                    placeholder="Filtro"
-                    className="input input-bordered input-sm w-36 ml-2"
-                    name="date"
-                    value={filters.date}
+                    name="hoursAbsent"
+                    placeholder="Filtrar por horas faltadas"
+                    value={filters.hoursAbsent}
                     onChange={handleFilterChange}
+                    className="input input-bordered w-full h-8"
                   />
                 </div>
+              </th>
+              <th className="w-1/5">
+                <div>
+                  <p className="pb-2 font-extrabold text-base">Fecha</p>
+                  <input
+                    type="text"
+                    name="date"
+                    placeholder="Filtrar por fecha"
+                    value={filters.date}
+                    onChange={handleFilterChange}
+                    className="input input-bordered w-full h-8"
+                  />
+                </div>
+              </th>
+              <th className="w-1/5 align-top font-extrabold text-base">
+                Acciones
               </th>
             </tr>
           </thead>
+
           <tbody>
             {filteredData.length > 0 ? (
               filteredData.map((item, index) => (
                 <tr key={index} className="hover:bg-base-100">
-                  <td>
-                    <div className="whitespace-nowrap max-w-xs">
-                      <div className="overflow-x-auto text-base">
-                        {item.name}
-                      </div>
-                    </div>
-                  </td>
+                  <td className="text-base">{item.name}</td>
                   <td className="text-base">{item.absenceType}</td>
-                  <td>
-                    <div className="whitespace-nowrap max-w-xs">
-                      <div className="overflow-x-auto text-base">
-                        {item.description}
-                      </div>
-                    </div>
-                  </td>
+                  <td className="text-base">{item.description}</td>
                   <td className="text-base">{item.hoursAbsent}</td>
                   <td className="text-base">{item.date}</td>
+                  <td>
+                    <button
+                      className="btn btn-error btn-sm mb-1"
+                      onClick={() => handleEditAbsence(item)}
+                    >
+                      Editar
+                    </button>
+                    <button
+                      className="btn btn-error btn-sm"
+                      onClick={() => handleDeleteAbsence(item.absenceId)}
+                    >
+                      Eliminar
+                    </button>
+                  </td>
                 </tr>
               ))
             ) : (
               <tr>
-                <td colSpan={5} className="text-center">
+                <td colSpan={6} className="text-center">
                   No hay datos
                 </td>
               </tr>
@@ -197,6 +267,96 @@ const AssistTable: React.FC = () => {
           </tbody>
         </table>
       </div>
+
+      <Modal
+        open={isModalOpen}
+        onClickBackdrop={() => setIsModalOpen(false)}
+        className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+      >
+        <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-md">
+          <Modal.Header className="font-bold text-lg pb-4">
+            Editar Ausencia
+          </Modal.Header>
+          <Modal.Body>
+            <input
+              type="text"
+              placeholder="Nombre"
+              className="input input-bordered w-full mb-4"
+              value={editingAbsence?.name || ''}
+              onChange={(e) =>
+                setEditingAbsence((prev) =>
+                  prev ? { ...prev, name: e.target.value } : prev
+                )
+              }
+            />
+            <select
+              className="select select-bordered w-full mb-4"
+              value={editingAbsence?.absenceType || ''}
+              onChange={(e) =>
+                setEditingAbsence((prev) =>
+                  prev ? { ...prev, absenceType: e.target.value } : prev
+                )
+              }
+            >
+              <option value="" disabled>
+                Selecciona el tipo de falta
+              </option>
+              {absenceTypes.map((type) => (
+                <option key={type.ABSENCE_TYPE_ID} value={type.TYPE}>
+                  {type.TYPE}
+                </option>
+              ))}
+            </select>
+            <input
+              type="text"
+              placeholder="Descripción"
+              className="input input-bordered w-full mb-4"
+              value={editingAbsence?.description || ''}
+              onChange={(e) =>
+                setEditingAbsence((prev) =>
+                  prev ? { ...prev, description: e.target.value } : prev
+                )
+              }
+            />
+            <input
+              type="number"
+              placeholder="Horas Faltadas"
+              className="input input-bordered w-full mb-4"
+              value={editingAbsence?.hoursAbsent || ''}
+              onChange={(e) =>
+                setEditingAbsence((prev) =>
+                  prev ? { ...prev, hoursAbsent: +e.target.value } : prev
+                )
+              }
+            />
+            <input
+              type="text"
+              placeholder="Fecha"
+              className="input input-bordered w-full"
+              value={editingAbsence?.date || ''}
+              onChange={(e) =>
+                setEditingAbsence((prev) =>
+                  prev ? { ...prev, date: e.target.value } : prev
+                )
+              }
+            />
+          </Modal.Body>
+          <Modal.Actions className="flex justify-end pt-4">
+            <button
+              className="btn btn-primary mr-2"
+              onClick={handleUpdateAbsence}
+            >
+              Guardar
+            </button>
+            <button
+              className="btn btn-secondary"
+              onClick={() => setIsModalOpen(false)}
+            >
+              Cancelar
+            </button>
+          </Modal.Actions>
+        </div>
+      </Modal>
     </div>
   );
 };
